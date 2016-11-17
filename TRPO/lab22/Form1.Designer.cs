17309690162d773b426891f2fc8016c559c66085
ZingerXY <Vitaliks90@mail.ru>
2016-11-17 03:13:05 +0300

    Добавил ТРПО

---
+++ TRPO/lab22/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace lab22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.addnew = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.change = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(589, 192);
+            this.dataGridView1.TabIndex = 34;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "performer_name";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(299, 217);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 50;
+            this.comboBox1.ValueMember = "performer_name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Авторы:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(125, 328);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 48;
+            this.checkBox1.Text = "Сольный";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // addnew
+            // 
+            this.addnew.Location = new System.Drawing.Point(130, 351);
+            this.addnew.Name = "addnew";
+            this.addnew.Size = new System.Drawing.Size(75, 23);
+            this.addnew.TabIndex = 47;
+            this.addnew.Text = "Добавить";
+            this.addnew.UseVisualStyleBackColor = true;
+            this.addnew.Click += new System.EventHandler(this.addnew_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(130, 380);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 46;
+            this.delete.Text = "Удалить";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(39, 380);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 45;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(436, 383);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(165, 23);
+            this.change.TabIndex = 44;
+            this.change.Text = "Изменить изображение";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(436, 223);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 43;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Время звучания";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Кол-во треков";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Год";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Название альбома";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(120, 298);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 38;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 272);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 37;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 246);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 36;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 220);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 35;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 430);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.addnew);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button addnew;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ TRPO/lab22/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab22
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection c = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=lab22.mdb");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = sql_query(c, "SELECT * FROM audio");
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].Visible = false;
+            for (int i = 1; i < 5; i++)
+            {
+                dataGridView1.Columns[i].HeaderText = Controls["label" + i].Text;
+                Controls["textBox" + i].DataBindings.Add("Text", dataGridView1.DataSource, dt.Columns[i].ToString());
+            }
+            dataGridView1.Columns[5].Visible = false;
+            pictureBox1.DataBindings.Add("Image", dataGridView1.DataSource, "album_img", true);
+            dataGridView1.Columns[6].HeaderText = "Сольный";
+            checkBox1.DataBindings.Add("Checked", dataGridView1.DataSource, dt.Columns[6].ToString());
+            dataGridView1.AutoResizeColumns();
+        }
+
+        private void addnew_Click(object sender, EventArgs e)
+        {
+            MemoryStream stream = new MemoryStream();
+            pictureBox1.Image.Save(stream , System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] img = stream.ToArray();
+            //if (sql_query(c, "INSERT INTO audio SET album_name='" + textBox1.Text + "', release_year=" + textBox2.Text + ", quantity_tracks=" + textBox3.Text + ", playing_time=" + textBox4.Text + ", album_img=@album_img", "@album_img", img) > 0)
+            if (sql_query(c, "INSERT INTO audio( album_name, release_year, quantity_tracks, playing_time, album_img) VALUES('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', @album_img)", "@album_img", img) > 0)
+                MessageBox.Show("Запись успешно добавлена.");
+        }
+
+        DataTable sql_query(OleDbConnection c, string query)
+        {
+            DataTable dt = new DataTable();
+            OleDbCommand mc = new OleDbCommand();
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            c.Open();
+            mc.Connection = c;
+            mc.CommandText = query;
+            da.SelectCommand = mc;
+            da.Fill(dt);
+            c.Close();
+            return dt;
+        }
+
+        int sql_query(OleDbConnection c, string query, string simg, byte[] img)
+        {
+            int n = 0;
+            DataTable dt = new DataTable();
+            OleDbCommand mc = new OleDbCommand();
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            c.Open();
+            mc.Connection = c;
+            mc.CommandText = query;
+            mc.Parameters.AddWithValue(simg, img);
+            n = mc.ExecuteNonQuery();
+            c.Close();
+            return n;
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            DataTable dt = sql_query(c, "SELECT * FROM Disc WHERE id=" + id);
+            /*
+                SELECT performer.id, performer.perf_name, performer.style, performer.country 
+                FROM Disc, performer
+                WHERE Disc.id =  AND performer.id = Disc.id_perf
+             */
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                DataTable dtb = sql_query(c, "SELECT * FROM performer WHERE id=" + dt.Rows[1][j].ToString());
+                string str = dtb.Rows[0][0].ToString();
+                comboBox1.Items.Add(str);
+            }
+            //Text = dt.Rows[0][0].ToString();
+        }
+
+
+
+
+        private void textBox0_TextChanged(object sender, EventArgs e)
+        {
+            /**/
+        }
+    }
+}
