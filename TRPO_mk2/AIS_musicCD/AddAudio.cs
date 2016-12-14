c596764acfdd0691205a4e25a88a6dcb741e3929
yourpowerro <yourpowerro@ya.ru>
2016-12-14 18:25:34 +0300

    В форму AddAudio (CD->добавить) добавлена загрузка авторов в listbox и combobox. Автоматическое заполнение combobox при выделении итема в listbox

--- TRPO_mk2/AIS_musicCD/AddAudio.cs
+++ TRPO_mk2/AIS_musicCD/AddAudio.cs
@@ -14,12 +14,16 @@ namespace AIS_musicCD
 	public partial class AddAudio : Form
 	{
 		OleDbConnection DBC;
+        DataTable dt;
 
-		public AddAudio(OleDbConnection DBC)
+
+        public AddAudio(OleDbConnection DBC)
 		{
 			this.DBC = DBC;
 			InitializeComponent();
-		}
+            authors_combobox();
+
+        }
 
 		private void change_Click(object sender, EventArgs e)
 		{
@@ -27,5 +31,23 @@ namespace AIS_musicCD
 			if (ofd.ShowDialog() == DialogResult.OK)
 				pictureBox1.Image = Image.FromFile(ofd.FileName);
 		}
-	}
+
+        //Добавляем авторов в комбобокс и в листбокс
+        private void authors_combobox()
+        {
+            dt = SQL.query(DBC, "Select authors.group_name from authors");
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                string str = dt.Rows[j][0].ToString();
+                comboBox1.Items.Add(str);
+                listBox1.Items.Add(str);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = listBox1.SelectedIndex;
+        }
+    }
 }
