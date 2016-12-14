17b11c903a4ae2e46728dd77c839f8fec7ec4cf8
ZingerXY <Vitaliks90@mail.ru>
2016-12-14 20:26:29 +0300

    Удаление лишних полей

--- TRPO_mk2/AIS_musicCD/Country.Designer.cs
+++ TRPO_mk2/AIS_musicCD/Country.Designer.cs
@@ -30,8 +30,6 @@
 		{
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.AddNew = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -46,14 +44,14 @@
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.ReadOnly = true;
 			this.dataGridView1.RowHeadersVisible = false;
-			this.dataGridView1.Size = new System.Drawing.Size(180, 192);
+			this.dataGridView1.Size = new System.Drawing.Size(210, 192);
 			this.dataGridView1.TabIndex = 36;
 			// 
 			// AddNew
 			// 
 			this.AddNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.AddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.AddNew.Location = new System.Drawing.Point(45, 217);
+			this.AddNew.Location = new System.Drawing.Point(58, 217);
 			this.AddNew.Name = "AddNew";
 			this.AddNew.Size = new System.Drawing.Size(114, 33);
 			this.AddNew.TabIndex = 37;
@@ -61,33 +59,11 @@
 			this.AddNew.UseVisualStyleBackColor = true;
 			this.AddNew.Click += new System.EventHandler(this.AddNew_Click);
 			// 
-			// textBox1
-			// 
-			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBox1.Location = new System.Drawing.Point(198, 34);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(114, 26);
-			this.textBox1.TabIndex = 38;
-			this.textBox1.Visible = false;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(194, 11);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(118, 20);
-			this.label1.TabIndex = 39;
-			this.label1.Text = "Новая страна:";
-			this.label1.Visible = false;
-			// 
 			// Country
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(204, 262);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox1);
+			this.ClientSize = new System.Drawing.Size(234, 262);
 			this.Controls.Add(this.AddNew);
 			this.Controls.Add(this.dataGridView1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -97,7 +73,6 @@
 			this.Load += new System.EventHandler(this.Country_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -105,7 +80,5 @@
 
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.Button AddNew;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Label label1;
 	}
 }
--- TRPO_mk2/AIS_musicCD/Style.Designer.cs
+++ TRPO_mk2/AIS_musicCD/Style.Designer.cs
@@ -28,38 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.AddNew = new System.Windows.Forms.Button();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(194, 11);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(112, 20);
-			this.label1.TabIndex = 43;
-			this.label1.Text = "Новый стиль:";
-			this.label1.Visible = false;
-			// 
-			// textBox1
-			// 
-			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBox1.Location = new System.Drawing.Point(198, 34);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(114, 26);
-			this.textBox1.TabIndex = 42;
-			this.textBox1.Visible = false;
-			// 
 			// AddNew
 			// 
 			this.AddNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.AddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.AddNew.Location = new System.Drawing.Point(45, 217);
+			this.AddNew.Location = new System.Drawing.Point(58, 217);
 			this.AddNew.Name = "AddNew";
 			this.AddNew.Size = new System.Drawing.Size(114, 33);
 			this.AddNew.TabIndex = 41;
@@ -78,16 +56,14 @@
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.ReadOnly = true;
 			this.dataGridView1.RowHeadersVisible = false;
-			this.dataGridView1.Size = new System.Drawing.Size(180, 192);
+			this.dataGridView1.Size = new System.Drawing.Size(210, 192);
 			this.dataGridView1.TabIndex = 40;
 			// 
 			// Style
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(204, 262);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox1);
+			this.ClientSize = new System.Drawing.Size(234, 262);
 			this.Controls.Add(this.AddNew);
 			this.Controls.Add(this.dataGridView1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -97,14 +73,10 @@
 			this.Load += new System.EventHandler(this.Style_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button AddNew;
 		private System.Windows.Forms.DataGridView dataGridView1;
 	}
