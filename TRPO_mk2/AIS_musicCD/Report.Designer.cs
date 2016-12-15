6c5e29f84e42667fb6d6acbe08485cef3b8bad84
ZingerXY <Vitaliks90@mail.ru>
2016-12-15 20:46:06 +0300

    Пример реализации отчета

--- TRPO_mk2/AIS_musicCD/Report.Designer.cs
+++ TRPO_mk2/AIS_musicCD/Report.Designer.cs
@@ -28,37 +28,94 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
 			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
 			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// chart1
 			// 
-			chartArea1.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea1);
-			legend1.Name = "Legend1";
-			this.chart1.Legends.Add(legend1);
-			this.chart1.Location = new System.Drawing.Point(12, 12);
+			chartArea3.Area3DStyle.Enable3D = true;
+			chartArea3.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea3);
+			legend2.Name = "Legend1";
+			this.chart1.Legends.Add(legend2);
+			this.chart1.Location = new System.Drawing.Point(27, 27);
 			this.chart1.Name = "chart1";
-			series1.ChartArea = "ChartArea1";
-			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-			series1.Legend = "Legend1";
-			series1.Name = "Series1";
-			series1.YValuesPerPoint = 4;
-			this.chart1.Series.Add(series1);
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+			series3.Legend = "Legend1";
+			series3.Name = "Series1";
+			series3.YValuesPerPoint = 4;
+			this.chart1.Series.Add(series3);
 			this.chart1.Size = new System.Drawing.Size(658, 376);
 			this.chart1.TabIndex = 0;
 			this.chart1.Text = "chart1";
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(739, 502);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.chart1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 29);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(731, 469);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Солные и сборники";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.chart2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 29);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(731, 469);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Стили";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// chart2
+			// 
+			chartArea4.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea4);
+			this.chart2.Location = new System.Drawing.Point(27, 27);
+			this.chart2.Name = "chart2";
+			series4.ChartArea = "ChartArea1";
+			series4.Name = "Series1";
+			series4.YValuesPerPoint = 4;
+			this.chart2.Series.Add(series4);
+			this.chart2.Size = new System.Drawing.Size(658, 376);
+			this.chart2.TabIndex = 2;
+			this.chart2.Text = "chart2";
 			// 
 			// Report
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(682, 400);
-			this.Controls.Add(this.chart1);
+			this.ClientSize = new System.Drawing.Size(736, 500);
+			this.Controls.Add(this.tabControl1);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -67,7 +124,12 @@
 			this.Name = "Report";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Report";
+			this.Load += new System.EventHandler(this.Report_Load);
 			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -75,5 +137,9 @@
 		#endregion
 
 		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
 	}
 }
--- TRPO_mk2/AIS_musicCD/Report.cs
+++ TRPO_mk2/AIS_musicCD/Report.cs
@@ -20,5 +20,27 @@ namespace AIS_musicCD
 			this.DBC = DBC;
 			InitializeComponent();
 		}
+
+		private void ShowReport()
+		{
+			int sol = 20;
+			int gr = 50;
+			chart1.Titles.Add("Сольные записи и сборники");
+			chart1.Series[0].Points.AddY(sol);
+			chart1.Series[0].Points[0].Label = sol + " Сольные";
+			chart1.Series[0].Points.AddY(gr);
+			chart1.Series[0].Points[1].Label = gr + " Cборники";
+
+			int clasic = 10;
+			int rock = 20;
+			chart2.Titles.Add("Стили");
+			chart2.Series[0].Points.AddXY("Класика", clasic);
+			chart2.Series[0].Points.AddXY("Рок", rock);
+        }
+
+		private void Report_Load(object sender, EventArgs e)
+		{
+			ShowReport();
+        }
 	}
 }
